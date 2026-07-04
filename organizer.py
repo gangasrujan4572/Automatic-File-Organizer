d1a67af065e3b3af440223e3979b01d8c71e35de
M GANGA SRUJAN <gangasrujanm@gmail.com>
2026-07-05 00:24:32 +0530

    Handle duplicate filenames automatically

--- organizer.py
+++ organizer.py
@@ -12,6 +12,16 @@ FILE_TYPES = {
     "Archives": [".zip", ".rar", ".7z"],
 }
 
+def get_unique_filename(destination_folder, filename):
+    name, extension = os.path.splitext(filename)
+    new_filename = filename
+    counter = 1
+
+    while os.path.exists(os.path.join(destination_folder, new_filename)):
+        new_filename = f"{name}({counter}){extension}"
+        counter += 1
+
+    return new_filename
 
 def organize_file(file_path):
     if os.path.isdir(file_path):
@@ -26,11 +36,12 @@ def organize_file(file_path):
             destination_folder = os.path.join(SOURCE_FOLDER, folder)
             os.makedirs(destination_folder, exist_ok=True)
 
-            destination = os.path.join(destination_folder, file_name)
+            unique_name = get_unique_filename(destination_folder, file_name)
+            destination = os.path.join(destination_folder, unique_name)
 
             shutil.move(file_path, destination)
 
-            print(f"✅ Moved: {file_name} → {folder}")
+            print(f"✅ Moved: {file_name} → {folder}/{unique_name}")
             return
 
 
