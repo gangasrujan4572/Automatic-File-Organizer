37f31d873b76b317d68f76e300905ab9275b1e3b
M GANGA SRUJAN <gangasrujanm@gmail.com>
2026-07-05 00:34:16 +0530

    Add activity logging

--- organizer.py
+++ organizer.py
@@ -1,7 +1,18 @@
+from email import message
 import os
 import shutil
+import logging
 
 SOURCE_FOLDER = os.path.join(os.getcwd(), "test")
+
+LOG_FOLDER = "logs"
+os.makedirs(LOG_FOLDER, exist_ok=True)
+
+logging.basicConfig(
+    filename=os.path.join(LOG_FOLDER, "organizer.log"),
+    level=logging.INFO,
+    format="%(asctime)s - %(message)s"
+)
 
 FILE_TYPES = {
     "Images": [".jpg", ".jpeg", ".png", ".gif", ".bmp"],
@@ -39,18 +50,37 @@ def organize_file(file_path):
             unique_name = get_unique_filename(destination_folder, file_name)
             destination = os.path.join(destination_folder, unique_name)
 
-            shutil.move(file_path, destination)
+            try:
+                shutil.move(file_path, destination)
+                message = f"Moved: {file_name} → {folder}/{unique_name}"
 
-            print(f"✅ Moved: {file_name} → {folder}/{unique_name}")
+                print(f"✅ {message}")
+                logging.info(message)
+            except Exception as e:
+                error = f"Error moving {file_name}: {e}"
+                print(f"❌ {error}")
+                logging.error(error)
             return
 
 
 def organize_existing_files():
     print("Scanning existing files...")
+    logging.info("========== File Organizer Started ==========")
+
+    count = 0
 
     for file in os.listdir(SOURCE_FOLDER):
         file_path = os.path.join(SOURCE_FOLDER, file)
-        organize_file(file_path)
+
+        if os.path.isfile(file_path):
+            organize_file(file_path)
+            count += 1
+
+    print(f"\n📊 Scan Complete!")
+    print(f"📁 Files processed: {count}")
+
+    logging.info(f"Scan completed. Files processed: {count}")
+    logging.info("========== File Organizer Finished ==========\n")
 
 
 if __name__ == "__main__":
